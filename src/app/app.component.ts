7993511d3c05a73f161ca24c85274053a10d0edf
LokeshKumarkumawat <77303293+LokeshKumarkumawat@users.noreply.github.com>
2024-04-14 13:01:15 +0530

    INITIAL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HttpClientModule ,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finalssr';
+
+  httpClient = inject(HttpClient)
+
+  data: any[] = [];
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+
+
+
+  fetchData() {
+    this.httpClient
+      .get('http://localhost:3000/products?page=1')
+      .subscribe((data: any) => {
+        console.log("DATA", data);
+        this.data = data.results;
+      });
+  }
+
 }
